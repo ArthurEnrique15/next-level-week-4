d8daeffb72bec104fcb9de8c9ad664b2610e68ec
Arthur <arthurenrique1512@gmail.com>
2021-02-24 19:44:51 -0300

    Aula 3 - realizando testes automatizados na aplicação utilizando jest

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,18 @@
-import { createConnection } from 'typeorm'
+import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 
-createConnection();
+export default async (): Promise<Connection> => {
+
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database: 
+                // IF TERNÁRIO
+                // ? COMANDO A SER EXECUTADO NO IF
+                // : COMANDO A SER EXECUTADO NO ELSE
+                process.env.NODE_ENV === 'test' 
+                ? "./src/database/database.test.sqlite"
+                : defaultOptions.database
+        })
+    );
+};
